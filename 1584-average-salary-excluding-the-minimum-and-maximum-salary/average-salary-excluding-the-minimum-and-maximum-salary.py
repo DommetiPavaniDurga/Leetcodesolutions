e1b0cb945254f438070a7c613b5262c2a008aaa5
DommetiPavaniDurga <dommetipavanidurga@gmail.com>
2026-05-24 20:29:59 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (17.46%) - LeetSync

---
+++ 1584-average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def average(self, salary: List[int]) -> float:
+        total = sum(salary)
+        minimum = min(salary)
+        maximum = max(salary)
+
+        return (total - minimum - maximum) / (len(salary) - 2)
